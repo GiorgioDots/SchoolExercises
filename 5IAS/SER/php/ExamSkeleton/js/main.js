f8081b5cd54465319cec5168af304cca97cb82b4
Giorgio Dodesini <giorgio.dodesini.it@gmail.com>
2020-05-09 14:58:41 +0200

    structure done

--- 5IAS/SER/php/ExamSkeleton/js/main.js
+++ 5IAS/SER/php/ExamSkeleton/js/main.js
@@ -1,11 +1,11 @@
 function onSelectAction(action) {
-  const selectQueryDiv = document.getElementById("selectQueryDiv");
-  const insertQueryDiv = document.getElementById("insertQueryDiv");
-  selectQueryDiv.innerHTML = "";
-  insertQueryDiv.innerHTML = "";
-  insertQueryDiv.className = ""
-  selectQueryDiv.className = "";
-  if (action === "query") {
+  const selectQueryDiv = document.getElementById('selectQueryDiv');
+  const insertQueryDiv = document.getElementById('insertQueryDiv');
+  selectQueryDiv.innerHTML = '';
+  insertQueryDiv.innerHTML = '';
+  insertQueryDiv.className = '';
+  selectQueryDiv.className = '';
+  if (action === 'query') {
     selectQueryDiv.innerHTML = `
       <select class="form-control" name="preselected_query" onchange="onChangeQuery(this.value)">
         <option value="insertOne">Insert One</option>
@@ -17,30 +17,30 @@ function onSelectAction(action) {
     insertQueryDiv.innerHTML = `
       <input type="text" class="form-control" name="inserted_query" placeholder="Insert a query">
     `;
-    insertQueryDiv.className = "form-group mr-sm-3"
-    selectQueryDiv.className = "form-group mr-sm-3";
+    insertQueryDiv.className = 'form-group mr-sm-3';
+    selectQueryDiv.className = 'form-group mr-sm-3';
   }
 }
 
 function onChangeQuery(action) {
-  const insertQueryDiv = document.getElementById("insertQueryDiv");
+  const insertQueryDiv = document.getElementById('insertQueryDiv');
   switch (action) {
-    case "query1":
-      let query1 = "SELECT * FROM Actors";
+    case 'query1':
+      let query1 = 'SELECT * FROM actor';
       insertQueryDiv.innerHTML = `
         <input type="hidden" name="query" value="${query1}">
         <p id="query"><code>${query1}</code></p>
       `;
       break;
-    case "query2":
-      let query2 = "SELECT * FROM Cinema";
+    case 'query2':
+      let query2 = 'SELECT * FROM cinema';
       insertQueryDiv.innerHTML = `
         <input type="hidden" name="query" value="${query2}">
         <p class="pt-3" id="query"><code>${query2}</code></p>
       `;
       break;
-    case "query3":
-      let query3 = "SELECT * FROM Film";
+    case 'query3':
+      let query3 = 'SELECT * FROM film';
       insertQueryDiv.innerHTML = `
         <input type="hidden" name="query" value="${query3}">
         <p id="query"><code>${query3}</code></p>
--- 5IAS/SER/php/ExamSkeleton/mysql/index.php
+++ 5IAS/SER/php/ExamSkeleton/mysql/index.php
@@ -1,24 +1,33 @@
 <?php
-  class MySQLDriver {
-    private $connection;
-    private $nations;
-    private $names;
-    private $surnames;
-    
-    public function __construct($configPath) {
-      $jConfig = file_get_contents($configPath);
-      $config = json_decode($jConfig, true);
-      $jUtils = file_get_contents('./utils.json');
-      $utils = json_decode($jUtils, true);
-      $this->connection = new mysqli($config["DB_HOST"],$config["DB_USER"],$config["DB_PASSWORD"],$config["DB_NAME"]);
-      $this->nations = $utils["NATIONS"];
-      $this->names = $utils["NAMES"];
-      $this->surnames = $utils["SURNAMES"];
-      $this->populateActors(10);
-    }
+class MySQLDriver
+{
+  private $connection;
+  private $nations;
+  private $names;
+  private $surnames;
+  private $films;
+  private $cities;
 
-    public function createTables(){
-      $queries = array("
+  public function __construct($configPath)
+  {
+    $jConfig = file_get_contents($configPath);
+    $config = json_decode($jConfig, true);
+    $jUtils = file_get_contents('./utils.json');
+    $utils = json_decode($jUtils, true);
+    $jFilms = file_get_contents('./films.json');
+    $films = json_decode($jFilms, true);
+    $this->connection = new mysqli($config["DB_HOST"], $config["DB_USER"], $config["DB_PASSWORD"], $config["DB_NAME"]);
+    $this->nations = $utils["NATIONS"];
+    $this->names = $utils["NAMES"];
+    $this->surnames = $utils["SURNAMES"];
+    $this->cities = $utils["CITIES"];
+    $this->films = $films;
+  }
+
+  public function createTables()
+  {
+    $queries = array(
+      "
         CREATE TABLE IF NOT EXISTS Actor(
           actor_id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
           surname TEXT NOT NULL, 
@@ -80,75 +89,132 @@
             ON UPDATE CASCADE ON DELETE RESTRICT
         )
       ",
-      );
-      $errorIndex = array();
-      foreach($queries as $index=>$query) {
-        try{
-          if($this->connection->query($query) != 1) {
-            array_push($errorIndex, $index);
-          }
-        }catch(Exception $e){
+    );
+    $errorIndex = array();
+    foreach ($queries as $index => $query) {
+      try {
+        if ($this->connection->query($query) != 1) {
           array_push($errorIndex, $index);
         }
-      }
-      if(sizeof($errorIndex) > 0){
-        throw new Exception("Query: ".$queries[$errorIndex[0]]." - went wrong;");
-      }
-      return "Tables created!";
-    }
-
-    public function populateTables($numRows = 1000) {
-      $this->populateActors($numRows);
-      $this->populateFilm($numRows);
-      $this->populateCinema($numRows);
-      $this->populateInterprets($numRows);
-      $this->populatePlanner($numRows);
-    }
-
-    private function populateActors($numRows) {
-      $genders = array('male', 'female'); // Non per offendere altri generi
-      $sql = "";
-      for($i = 0; $i < $numRows; $i++){
-        $name = $this->names[array_rand($this->names)];
-        $surname = $this->surnames[array_rand($this->surnames)];
-        $gender = $genders[array_rand($genders, 1)];
-        $birthday = $this->generateRandomDate();
-        $nationality = $this->nations[array_rand($this->nations)];
-        $sql = "INSERT INTO `actor`(`surname`, `name`, `gender`, `birthday`, `nationality`) VALUES ('$surname', '$name', '$gender', '$birthday', '$nationality');";
-        $this->connection->query($sql);
+      } catch (Exception $e) {
+        array_push($errorIndex, $index);
       }
     }
-
-    private function populateFilm($numRows) {
-
+    if (sizeof($errorIndex) > 0) {
+      throw new Exception("Query: " . $queries[$errorIndex[0]] . " - went wrong;");
     }
+    return "Tables created!";
+  }
 
-    private function populateCinema($numRows) {
+  public function populateTables($numRows = 1000)
+  {
+    $this->populateActors($numRows);
+    $this->populateFilm($numRows);
+    $this->populateCinema($numRows);
+    $this->populateInterprets($numRows);
+    $this->populatePlanner($numRows);
+  }
 
-    }
+  public function executeQuery($query)
+  {
+    $result = $this->connection->query($query);
+    return $result->fetch_all(MYSQLI_ASSOC);
+  }
 
-    private function populateInterprets($numRows) {
-
-    }
-
-    private function populatePlanner($numRows) {
-
-    }
-
-    private function generateRandomString($length = 10) {
-      $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-      $charactersLength = strlen($characters);
-      $randomString = '';
-      for ($i = 0; $i < $length; $i++) {
-          $randomString .= $characters[rand(0, $charactersLength - 1)];
-      }
-      return $randomString;
-    }
-    private function generateRandomDate() {
-      $start = strtotime("01 Jenuary 1940");
-      $end = strtotime("22 July 2010");
-      $timestamp = mt_rand($start, $end);
-      return date("Y-m-d", $timestamp);
+  private function populateActors($numRows)
+  {
+    $genders = array('male', 'female'); // Non per offendere altri generi
+    $sql = "";
+    for ($i = 0; $i < $numRows; $i++) {
+      $name = $this->names[array_rand($this->names)];
+      $surname = $this->surnames[array_rand($this->surnames)];
+      $gender = $genders[array_rand($genders, 1)];
+      $birthday = $this->generateRandomDate();
+      $nationality = $this->nations[array_rand($this->nations)];
+      $sql = "INSERT INTO `actor`(`surname`, `name`, `gender`, `birthday`, `nationality`) VALUES ('$surname', '$name', '$gender', '$birthday', '$nationality');";
+      $this->connection->query($sql);
     }
   }
-?> 
+
+  private function populateFilm($numRows)
+  {
+    for ($i = 0; $i < $numRows; $i++) {
+      $film = $this->films[array_rand($this->films)];
+      $title = $film["title"];
+      $prodYear = $film["year"];
+      $prodPlace = $this->nations[array_rand($this->nations)];
+      $dirSurname = $this->surnames[array_rand($this->surnames)];
+      $genres = $film["genres"];
+      if ($genres) {
+        $genre = $genres[0];
+      } else {
+        $genre = "Adventure";
+      }
+      $sql = "INSERT INTO `film`(`title`, `production_year`, `production_place`, `director_surname`, `genre`) VALUES ('$title', '$prodYear', '$prodPlace', '$dirSurname', '$genre')";
+      $this->connection->query($sql);
+    }
+  }
+
+  private function populateCinema($numRows)
+  {
+    for ($i = 0; $i < $numRows; $i++) {
+      $name = $this->surnames[array_rand($this->surnames)];
+      $seats = rand(50, 300);
+      $city = $this->cities[array_rand($this->cities)];
+      $sql = "INSERT INTO `cinema`(`name`, `seats`, `city`) VALUES ('$name','$seats','$city')";
+      $this->connection->query($sql);
+    }
+  }
+
+  private function populateInterprets($numRows)
+  {
+    for ($k = 0; $k < $numRows; $k++) {
+      $fetchActorsQuery = "SELECT actor_id FROM actor";
+      $actors = $this->connection->query($fetchActorsQuery);
+      $max = rand(1, $actors->num_rows);
+      for ($i = 0; $i < $max; $i++) {
+        $actor = $actors->fetch_assoc();
+      }
+      $fetchFilmQuery = "SELECT film_id FROM film";
+      $films = $this->connection->query($fetchFilmQuery);
+      $max = rand(1, $films->num_rows);
+      for ($i = 0; $i < $max; $i++) {
+        $film = $films->fetch_assoc();
+      }
+      $name = $this->names[array_rand($this->names)];
+      $surname = $this->surnames[array_rand($this->surnames)];
+      $sql = "INSERT INTO `interprets`(`actor_id_fk`, `film_id_fk`, `character`) VALUES ({$actor['actor_id']}, {$film['film_id']}, '$name $surname')";
+      $this->connection->query($sql);
+    }
+  }
+
+  private function populatePlanner($numRows)
+  {
+    for ($k = 0; $k < $numRows; $k++) {
+      $fetchCinemasQuery = "SELECT cinema_id FROM cinema";
+      $cinemas = $this->connection->query($fetchCinemasQuery);
+      $max = rand(1, $cinemas->num_rows);
+      for ($i = 0; $i < $max; $i++) {
+        $cinema = $cinemas->fetch_assoc();
+      }
+      $fetchFilmQuery = "SELECT film_id FROM film";
+      $films = $this->connection->query($fetchFilmQuery);
+      $max = rand(1, $films->num_rows);
+      for ($i = 0; $i < $max; $i++) {
+        $film = $films->fetch_assoc();
+      }
+      $takings = rand(1000000, 1000000000);
+      $projectionDate = $this->generateRandomDate();
+      $sql = "INSERT INTO `planned`(`film_id_fk`, `cinema_id_fk`, `takings`, `projection_date`) VALUES ({$film['film_id']},{$cinema['cinema_id']},$takings, '$projectionDate')";
+      $this->connection->query($sql);
+    }
+  }
+
+  private function generateRandomDate()
+  {
+    $start = strtotime("01 Jenuary 1940");
+    $end = strtotime("22 July 2010");
+    $timestamp = mt_rand($start, $end);
+    return date("Y-m-d", $timestamp);
+  }
+}
